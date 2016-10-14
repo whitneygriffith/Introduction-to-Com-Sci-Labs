793e6911bd61e05b545df7a1db26c3ea3d062c44
Whitney Griffith <whitney.griffith@bison.howard.edu>
2016-10-13 21:57:20 -0400

    Add files via upload

--- count_vowels.py
+++ count_vowels.py
@@ -1,2 +1,13 @@
-def GetNthLargestNumber(nums, n):
-    
+def CountVowels(string):
+    count = 0
+    for i in string:
+        if i == 'a' or i == 'e' or i == 'i' or i == 'o' or i == 'u' :
+            count += 1
+    print(count)
+
+
+CountVowels('pizza')
+CountVowels('i like ike')
+CountVowels('today was a good day')
+CountVowels('grrrrrrrrrr')
+CountVowels('')
